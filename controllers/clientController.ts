67e2f3c9753a19a61277e01ee73ec87586172ed1
Itamar Junior <cdajuniorf@gmail.com>
2025-02-05 02:03:49 -0300

    feat: Trabalhando com controllers.

--- controllers/clientController.ts
+++ controllers/clientController.ts
@@ -4,4 +4,28 @@ function index(req: Request, res: Response, next: any) {
   res.render("index");
 }
 
-export default { index };
+function produto(req: Request, res: Response, next: any) {
+  var produtos = [
+    {
+      id: 1,
+      produto: "Memória Ram",
+      descricao: "16GB",
+    },
+    {
+      id: 2,
+      produto: "Placa de video",
+      descricao: "8GB",
+    },
+    {
+      id: 3,
+      produto: "Monitor",
+      descricao: "fullHD",
+    },
+  ];
+  res.send(
+    `<h1>${produtos[parseInt(req.params.id)].produto}</h1> 
+      <p>${produtos[parseInt(req.params.id)].descricao}</p>`
+  );
+}
+
+export default { index, produto };
--- routes/clients.ts
+++ routes/clients.ts
@@ -4,36 +4,6 @@ import clientsController from "../controllers/clientController";
 const router = Router();
 
 router.get("/", clientsController.index);
-
-router.get("/saudacao/:nome?", (req, res) => {
-  if (req.params.nome === undefined) {
-    res.send("TreinaWeb");
-  } else {
-    res.send(`<h1>Ola ${req.params.nome}</h1>`);
-  }
-});
-router.get("/produto/:id", (req, res) => {
-  var produtos = [
-    {
-      id: 1,
-      produto: "Memória Ram",
-      descricao: "16GB",
-    },
-    {
-      id: 2,
-      produto: "Placa de video",
-      descricao: "8GB",
-    },
-    {
-      id: 3,
-      produto: "Monitor",
-      descricao: "fullHD",
-    },
-  ];
-  res.send(
-    `<h1>${produtos[parseInt(req.params.id)].produto}</h1> 
-      <p>${produtos[parseInt(req.params.id)].descricao}</p>`
-  );
-});
+router.get("/produto/:id", clientsController.produto);
 
 export default router;
